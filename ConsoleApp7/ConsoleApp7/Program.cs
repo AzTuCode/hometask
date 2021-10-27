7a039416ef57dc57006dbd69b32d2120e0a29f5c
AzTuCode <tu201806158@code.edu.az>
2021-10-28 01:47:58 +0400

    update

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+         int count=   GetWordCount(input);
+            Console.WriteLine(count);
+        }
+        static int GetWordCount (string param)
+        {
+            int count = 0;
+            for (int i = 0; i < param.Length; i++)
+            {
+                if (param[i]==' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("string daxil edin");
+            string param = Console.ReadLine();
+            Console.WriteLine("simvol daxil edin");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            int[] array = GetArray(param, symbol);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i]==0 && i==0)|| array[i]!=0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        static int [] GetArray(string param,char symbol)
+        {
+            int count = 0;
+            int[] result = new int[param.Length];
+
+            for (int i = 0; i < param.Length; i++)
+            {
+                if (param[i]==symbol)
+                {
+                    result[count] = i;
+                    count++;
+
+                }
+            }
+      
+            return result;
+        }
+    }
+}
